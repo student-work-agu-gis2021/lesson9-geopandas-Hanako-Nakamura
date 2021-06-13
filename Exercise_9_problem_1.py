aa12faf7373e05f83007f4f0396f832acbd09401
HanakoNakamura <81952361+Hanako-Nakamura@users.noreply.github.com>
2021-06-13 16:10:52 +0000

    I read and write spatial data in Python.

--- Exercise_9_problem_1.py
+++ Exercise_9_problem_1.py
@@ -27,12 +27,14 @@ latitudes = [63.748023986816406, 62.90789794921875, 60.511383056640625, 60.44499
 
 
 # Create a list of coordinate pairs
+
 # YOUR CODE HERE 1 
-coordpairs=None
+coordpairs = list(zip(longitudes,latitudes))
 
 # Create a shapely Polygon using the 'coordpairs' -list
+
 # YOUR CODE HERE 2 
-poly = None
+poly = Polygon(coordpairs)
 
 # CODE FOR TESTING YOUR SOLUTION
 
@@ -48,7 +50,11 @@ print(poly.geom_type)
 
 # Create an empty GeoDataFrame
 geo = None
+
 # YOUR CODE HERE 3
+geo = gpd.GeoDataFrame(index = [0] , columns =['geometry']
+)
+geo['geometry'] = poly
 
 # CODE FOR TESTING YOUR SOLUTION
 
@@ -65,7 +71,10 @@ print(len(geo))
 # - plot the polygon. Note: you might need to call `matplotlib pyplot show()` -method in order to display the map
 
 # Plot the polygon. What shape is it :) ?
+
 # YOUR CODE HERE 4
+geo.plot()
+plt.show()
 
 # What can you see on the map? :) 
 
@@ -75,6 +84,7 @@ print(len(geo))
 fp = 'polygon.shp'
 
 # YOUR CODE HERE 5
+geo.to_file(fp)
 
 # CODE FOR TESTING YOUR SOLUTION
 
@@ -96,4 +106,3 @@ def func3():
 
 def func4():
     return geo
-
